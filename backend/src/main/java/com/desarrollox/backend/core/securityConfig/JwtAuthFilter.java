95ab3a68f47ca4e44f567abe3980aa390b835e79
payan3521 <pablobedoya3521@gmail.com>
2025-12-06 18:05:48 -0500

    Solucionando inconsistencias en el backend

--- backend/src/main/java/com/desarrollox/backend/core/securityConfig/JwtAuthFilter.java
+++ backend/src/main/java/com/desarrollox/backend/core/securityConfig/JwtAuthFilter.java
@@ -51,7 +51,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             (path.equals("/api/posts/recent") && method.equals("GET")) ||
             (path.equals("/api/register") && method.equals("POST")) ||
             (path.equals("/api/sections") && method.equals("GET")) ||
-            (path.equals("/health") && method.equals("GET"))
+            (path.equals("/health") && method.equals("GET")) ||
+            (path.equals("/api/posts/{id}") && method.equals("GET")) ||
+            (path.equals("/api/models/{id}") && method.equals("GET"))
         ) 
             {
             filterChain.doFilter(request, response);
